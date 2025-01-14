1734c62f1278f54de5b535ed471041e09ce9ae99
Isaac Spencer <22ispencer@gmail.com>
2025-01-14 00:25:00 -0600

    replaced eslint with eslint_d

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,7 +1,7 @@
 Add("mfussenegger/nvim-lint")
 Later(function()
 	require("lint").linters_by_ft = {
-		javascript = { "eslint" },
+		javascript = { "eslint_d" },
 	}
 	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		callback = function()
