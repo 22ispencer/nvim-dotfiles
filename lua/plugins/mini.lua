3b1cdef5d0f26324bdad18aea13d0c7fc24dfca8
Isaac Spencer <22ispencer@gmail.com>
2025-01-13 22:58:12 -0600

    centering working

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -127,42 +127,54 @@ Now(function()
 			content_hooks = {
 				starter.gen_hook.adding_bullet("› "),
 				function(content)
-					local header_lines = vim.tbl_map(function(c)
-						return content[c.line][c.unit].string
-					end, starter.content_coords(content, "header"))
-					local header_width = math.max(unpack(vim.tbl_map(function(l)
-						return vim.fn.strdisplaywidth(l)
-					end, header_lines)))
+					-- Coords
+					local header_coords = starter.content_coords(content, "header")
+					local section_coords = starter.content_coords(content, "section")
+					local item_coords = starter.content_coords(content, "item")
+					local footer_coords = starter.content_coords(content, "footer")
 
-					local other_coords = starter.content_coords(content, function(unit)
-						return unit.type ~= "header" and unit.type ~= "footer"
-					end)
-					local other_lines = vim.tbl_map(function(c)
-						return content[c.line][c.unit].string
-					end, other_coords)
-					local other_width = math.max(unpack(vim.tbl_map(function(l)
-						return vim.fn.strdisplaywidth(l)
-					end, other_lines)))
+					-- Lines
+					local header_width = math.max(unpack(vim.tbl_map(function(c)
+						local line = content[c.line][c.unit].string
+						return vim.fn.strdisplaywidth(line)
+					end, header_coords)))
+					local section_width = math.max(unpack(vim.tbl_map(function(c)
+						local line = content[c.line][c.unit].string
+						return vim.fn.strdisplaywidth(line)
+					end, section_coords)))
+					local item_width = math.max(unpack(vim.tbl_map(function(c)
+						local line = content[c.line][c.unit].string
+						return vim.fn.strdisplaywidth(line)
+					end, item_coords)))
+					local footer_width = math.max(unpack(vim.tbl_map(function(c)
+						local line = content[c.line][c.unit].string
+						return vim.fn.strdisplaywidth(line)
+					end, footer_coords)))
+					local max_width = math.max(header_width, section_width, item_width, footer_width)
 
-					local pad_width = (header_width - other_width) * 0.5
-					local pad = string.rep(" ", pad_width)
+					for _, line in ipairs(content) do
+						if not (#line == 0 or (#line == 1 and line[1].string == "")) then
+							local line_str = ""
+							local line_types = {}
+							for _, unit in ipairs(line) do
+								line_str = line_str .. unit.string
+								table.insert(line_types, unit.type)
+							end
+							local line_width = 0
+							for _, type in ipairs(line_types) do
+								if type == "item" or type == "section" then
+									line_width = math.max(item_width, section_width)
+								elseif type == "header" then
+									line_width = header_width
+								elseif type == "footer" then
+									line_width = footer_width
+								end
+							end
+							local left_pad = string.rep(" ", (max_width - line_width) * 0.5)
 
-					local coords = starter.content_coords(content, function(unit)
-						return unit.type ~= "header" and unit.type ~= "footer" and unit.type ~= "item"
-					end)
-					for i = 1, #coords do
-						local l_num, u_num = coords[i].line, coords[i].unit
-						local centering_unit = {
-							string = pad,
-							type = "header_centering",
-							hl = nil,
-						}
-						table.insert(content[l_num], u_num, centering_unit)
+							table.insert(line, 1, { string = left_pad, type = "empty" })
+						end
 					end
-					-- put(content)
-					-- local lines_width = vim.tbl_map(function(l)
-					-- 	return vim.fn.strdisplaywidth(l)
-					-- end, line_strings)
 					return content
 				end,
 				starter.gen_hook.aligning("center", "center"),
