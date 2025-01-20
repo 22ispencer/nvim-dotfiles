7f539a407665390ee952b6f09cd6f9c9ba5ab99f
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-01-20 11:14:12 -0600

    added everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,9 @@
 Add("sainnhe/sonokai")
 vim.g.sonokai_enable_italic = true
 vim.g.sonokai_style = "shusia"
-vim.cmd.colorscheme("sonokai")
+
+Add("sainnhe/everforest")
+vim.g.everforest_enable_italic = true
+vim.g.everforest_background = "hard"
+
+vim.cmd.colorscheme("everforest")
