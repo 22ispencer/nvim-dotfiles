d6431bf7b2b0a5ed155eb67d006ac4344650533c
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-01-20 14:20:23 -0600

    changed neorg - changed the way neorg was configured   * deleted unnecessary lines (duplicates of defaults)   * disabled `bullets.vim` because of conflict with `neorg.promo` - `mini.jump2d`   * replaced comment with `enabled = false`

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -15,6 +15,29 @@ return {
 						default_workspace = "notes",
 					},
 				},
+				["core.esupports.metagen"] = {
+					config = {
+						author = "Isaac Spencer",
+						type = "auto",
+						update_date = true,
+					},
+				},
+				["core.export"] = {},
+				["core.export.markdown"] = { config = { extensions = "all" } },
+				["core.summary"] = {},
+				["core.tangle"] = {
+					config = {
+						report_on_empty = false,
+						tangle_on_write = true,
+					},
+				},
+				["core.ui.calendar"] = {},
+				["core.journal"] = {
+					config = {
+						workspace = "notes",
+					},
+				},
+				-- make lists nicer
 			},
 		})
 
