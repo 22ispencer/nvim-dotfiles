29149acf22558ea547acab28f4dd765ed6c6e2ff
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-01-21 09:55:31 -0600

    keybind to open calc

--- lua/plugins/calc.lua
+++ lua/plugins/calc.lua
@@ -1,1 +1,11 @@
-return { "22ispencer/calc.nvim", dev = true }
+return {
+	"22ispencer/calc.nvim",
+	dev = true,
+	keys = { {
+		"<leader>*",
+		function()
+			require("calc").open()
+		end,
+		desc = "open calc",
+	} },
+}
