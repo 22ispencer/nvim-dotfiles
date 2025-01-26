34e5a54f709bdd2125d478a6bcc9f50150ad325e
Isaac Spencer <22ispencer@gmail.com>
2025-01-26 09:59:11 -0600

    disabled sonokai options

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,8 +8,6 @@ return {
 			-- directly inside the plugin declaration.
 			vim.g.sonokai_enable_italic = true
 			vim.g.sonokai_style = "shusia"
-			vim.g.sonokai_transparent_background = 1
-			vim.g.sonokai_dim_inactive_windows = 1
 			vim.cmd.colorscheme("sonokai")
 		end,
 	},
