2afc1dee86fb9e057ed99fe7f06e9b346c78afba
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-02-21 12:47:14 -0600

    added ruff to linters

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,6 +3,7 @@ return {
 	config = function()
 		require("lint").linters_by_ft = {
 			javascript = { "eslint_d" },
+			python = { "ruff" },
 		}
 		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 			callback = function()
