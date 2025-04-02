341c05a8cc6d5179672b614ecdc1bb77113d7d73
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-04-02 13:07:08 -0500

    treesitter folding

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		vim.opt.foldenable = false
+
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = { "typescript", "html", "css", "lua", "markdown", "markdown_inline", "python" },
 			sync_install = false,
